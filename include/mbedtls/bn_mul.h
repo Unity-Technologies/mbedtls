9ab5c7dc0b131a0da1a72d562dc1db8d5444446b
Valdemar Bucilko <valdemar@unity3d.com>
2018-01-30 12:40:20 +0200

    Reformat according to coding guide

--- include/mbedtls/bn_mul.h
+++ include/mbedtls/bn_mul.h
@@ -845,11 +845,11 @@
     unsigned char carry;
 
 #define MULADDC_CORE                       \
-    r0 = _umul128(*(s++), b, &r1);         \
-    carry = _addcarry_u64(0, r0, c, &r0);  \
-    _addcarry_u64(carry, r1, 0, &r1);      \
-    carry = _addcarry_u64(0, r0, *d, &r0); \
-    _addcarry_u64(carry, r1, 0, &r1);      \
+    r0 = _umul128( *(s++), b, &r1 );         \
+    carry = _addcarry_u64( 0, r0, c, &r0 );  \
+    _addcarry_u64( carry, r1, 0, &r1 );      \
+    carry = _addcarry_u64( 0, r0, *d, &r0 ); \
+    _addcarry_u64( carry, r1, 0, &r1 );      \
     c = r1; *(d++) = r0;
 
 #define MULADDC_STOP                    \
