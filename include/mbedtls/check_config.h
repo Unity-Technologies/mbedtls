15fc71d3780b4fd11854fe9bfdd00bcea338cc94
Valdemar Bucilko <valdemar@unity3d.com>
2018-01-31 14:27:07 +0200

    x64 intrinsics for msvc in bn_mul, timing and aesni modules
    AES-NI implementation for MSVC x64 using intrinsics.
    Implement rdtsc timing function using intrinsics for x64.
    Use 128bit-result multiply on msvc/x64.

--- include/mbedtls/check_config.h
+++ include/mbedtls/check_config.h
@@ -66,7 +66,7 @@
 #error "MBEDTLS_HAVE_TIME_DATE without MBEDTLS_HAVE_TIME does not make sense"
 #endif
 
-#if defined(MBEDTLS_AESNI_C) && !defined(MBEDTLS_HAVE_ASM)
+#if defined(MBEDTLS_AESNI_C) && !defined(MBEDTLS_HAVE_ASM) && !defined(MBEDTLS_HAVE_INTRINSICS)
 #error "MBEDTLS_AESNI_C defined, but not all prerequisites"
 #endif
 
--- include/mbedtls/config.h
+++ include/mbedtls/config.h
@@ -54,7 +54,31 @@
  *
  * Comment to disable the use of assembly code.
  */
+#if !(defined(_MSC_VER) && defined(_M_X64))
 #define MBEDTLS_HAVE_ASM
+#endif
+
+
+/**
+* \def MBEDTLS_HAVE_INTRINSICS
+*
+* The compiler has support for intrinsic functions.
+*
+* Use this as an alternative to MBEDTLS_HAVE_ASM on compilers that don't
+* support inline assembly, but provide intrinsic functions for needed
+* instructions.
+*
+* Used in:
+*      include/mbedtls/bn_mul.h
+*      library/timing.c
+*      library/aesni.c
+*
+* Comment to disable the use of intrinsics in code.
+*/
+#if defined(_MSC_VER) && defined(_M_X64)
+#define MBEDTLS_HAVE_INTRINSICS
+#endif
+
 
 /**
  * \def MBEDTLS_NO_UDBL_DIVISION
@@ -91,7 +115,7 @@
  *
  * Uncomment if the CPU supports SSE2 (IA-32 specific).
  */
-//#define MBEDTLS_HAVE_SSE2
+#define MBEDTLS_HAVE_SSE2
 
 /**
  * \def MBEDTLS_HAVE_TIME
@@ -1543,7 +1567,7 @@
  * Module:  library/aesni.c
  * Caller:  library/aes.c
  *
- * Requires: MBEDTLS_HAVE_ASM
+ * Requires: MBEDTLS_HAVE_ASM or MBEDTLS_HAVE_X64_INTRINSICS
  *
  * This modules adds support for the AES-NI instructions on x86-64
  */
@@ -2181,7 +2205,9 @@
  *
  * This modules adds support for the VIA PadLock on x86.
  */
+#if !defined(_MSC_VER) && !defined(_M_X64)
 #define MBEDTLS_PADLOCK_C
+#endif
 
 /**
  * \def MBEDTLS_PEM_PARSE_C
