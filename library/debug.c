29af02920d0a3f7bbd449a9f6d811fe31da1146f
Kitty Draper <kitty.draper@unity3d.com>
2025-02-21 13:24:35 -0600

    Fix compile errors on Windows with strict warnings-as-errors settings.

--- library/debug.c
+++ library/debug.c
@@ -449,15 +449,7 @@ void mbedtls_debug_printf_ecdh(const mbedtls_ssl_context *ssl, int level,
                                const mbedtls_ecdh_context *ecdh,
                                mbedtls_debug_ecdh_attr attr)
 {
-#if defined(MBEDTLS_ECDH_LEGACY_CONTEXT)
     mbedtls_debug_printf_ecdh_internal(ssl, level, file, line, ecdh, attr);
-#else
-    switch (ecdh->var) {
-        default:
-            mbedtls_debug_printf_ecdh_internal(ssl, level, file, line, ecdh,
-                                               attr);
-    }
-#endif
 }
 #endif /* MBEDTLS_KEY_EXCHANGE_SOME_ECDH_OR_ECDHE_ANY_ENABLED &&
           MBEDTLS_ECDH_C */
